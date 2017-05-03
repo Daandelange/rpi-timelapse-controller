8fa5e9ee1b1009b3701a1e3e498e802ec71f63aa
Daan <idaany@gmail.com>
2017-05-03 15:06:42 +0000

    Timelapse tweaking

--- timelapse.sh
+++ timelapse.sh
@@ -5,7 +5,7 @@
 # run with sudo so it can reset itself via lsusb when gphoto crashes
 
 # Setup the pin that controls the light switch
-LIGHT_PIN=11
+LIGHT_PIN=0
 
 # timelapse trigger delay in seconds
 # real delay is DELAY_BETWEEN_SHOTS + trigger execution time
@@ -26,16 +26,66 @@ CAMERA_USB_VENDOR=04a9:3084
 # sudo update-rc.d rc.timelapse defaults
 
 # set gpio mode to write (switch control)
-/usr/local/bin/gpio mode $LIGHT_PIN out
+gpio mode $LIGHT_PIN out
 
 # cd into photo dir ( changeme )
 mkdir /home/pi/Desktop/gphoto_sessions/test3
 cd /home/pi/Desktop/gphoto_sessions/test3
 
+# set serial transfer speed / baud rate
+gphoto2 --speed=384000
+
 # setup gphoto2 (add you own preset commands)
-#gphoto2 --auto-detect
+
+# if not using autodetect, you must set --camera an --port
+gphoto2 --auto-detect
 #gphoto2 --camera MODEL
-gphoto2 --camera "Canon EOS 300D (normal mode)"
+#gphoto2 --camera="Canon EOS 300D (normal mode)"
+#gphoto2 --port="usb:001,006"
+
+# set capture speed to make sure
+# fit this to your cam. Use gphoto2 --list-config to get names
+# then use gphoto --get-config name to getpossible settings
+#gphoto2 --set-config name=value
+
+
+#/main/settings/shootingmode=4|M,8|Manual 2,0|auto,21|neutral
+gphoto2 --set-config-index /main/settings/shootingmode=4
+
+#/main/settings/ownername
+#/main/settings/capturesizeclass
+
+# /main/settings/iso=1|100,4|200,|400
+gphoto2 --set-config-index /main/settings/iso=1
+
+#/main/settings/shutterspeed=0|Bulb,30|1/125
+gphoto2 --set-config-index /main/settings/shutterspeed=30
+
+#/main/settings/zoom
+
+#/main/settings/aperture=10|4.0,4|2.0,25|22,16|8
+gphoto2 --set-config-index /main/settings/aperture=10
+
+#/main/settings/exposurecompensation
+
+# /main/settings/imageformat=7|Large Fine JPEG,2|Small Normal JPEG,0|RAW,1|RAW 2
+gphoto2 --set-config-index /main/settings/imageformat=7
+
+#/main/settings/focusmode=1|Auto focus: AI servo,3|Manual focus,2|Auto focus: AI focus,0|Auto focus: one-shot 
+gphoto2 --set-config-index /main/settings/focusmode=3
+
+#/main/settings/flashmode=0|Flash off,1|Flash on,2|Flash auto
+gphoto2 --set-config-index /main/settings/flashmode=0
+
+#/main/settings/beep
+#/main/actions/syncdatetime
+#/main/status/model
+#/main/status/datetime
+#/main/status/firmwareversion
+#/main/status/driver
+#/main/Driver/list_all_files
+
+#gphoto2 --mkdir "gphotoTimelapse"
 
 
 CONTINUE=1
@@ -47,7 +97,7 @@ do
 	echo "Taking photo $i (including $fails fails) ..."
 
 	# Turn lights on
-	/usr/local/bin/gpio write $LIGHT_PIN 1
+	gpio write $LIGHT_PIN 1
 	sleep 5
 
 	# Take photo!
@@ -59,10 +109,11 @@ do
 	# gives x seconds to command to execute a command
 	# call the command here! (take photo)
 	{
-		gphoto2 --capture-image-and-download
+		#gphoto2 --capture-image-and-download --no-keep
+		gphoto2 --capture-image --no-keep
 	} & pid=$!
 	# define timeout limit here
-	( sleep 40 && kill -HUP $pid ) 2>/dev/null & watcher=$!
+	( sleep 60 && kill -HUP $pid ) 2>/dev/null & watcher=$!
 	if wait $pid 2>/dev/null; then
 		# command terminated
 		echo "Photo taken successfully! :D"
@@ -74,12 +125,37 @@ do
 		wait $watcher
 	else
 		# timeout occured
-		echo "Oups, gphoto failed taking the photo within 40 seconds..."
+		echo "Oups, gphoto failed taking the photo within 60 seconds..."
 		echo "If this keeps failing, something might be wrong. (Firmware crash? No more batteries? etc.)"
+		
+		SUCCEEDED=false
+	fi
+
+	# shut lights anyways
+	sleep 1
+	gpio write $LIGHT_PIN 0
+
+	if [ "$SUCCEEDED" = true ]; then
+		# download last image
+		echo "Downloading image before continuing..."
+		#gphoto2 --get-file "first-last"
+		gphoto2 --get-file 1  --force-overwrite
+		gphoto2 --delete-file 1
+		#gphoto2 -d ""
+		#gphoto2 --get-all-files  --force-overwrite
+		#gphoto2 --delete-all-files
+		# max 10sec to wait for dl signal
+		#gphoto2 --wait-event-and-download=10
+		#gphoto2 --new
+	else
 		# reset camera with a lsusb reset
-		echo "Resetting USB connection now."
+		echo "Resetting USB connection now..."
 
 		$(lsusb -d 04a9:3084 | awk -F '[ :]' '{ print "/dev/bus/usb/"$2"/"$4 }' | xargs -I {} echo "/home/pi/GIT/rpi-timelapse-controller/usbreset {}")
+		# todo: use gphoto2 --reset
+
+		echo "Waiting a minimum of 60sec before retrying..."
+		sleep 60
 		
 		# restart gphoto and restart capturing
 		echo "Auto detecting cameras..."
@@ -89,9 +165,6 @@ do
 		# sudo shutdown -h -F now -r
 	fi
 
-	# shut lights
-	sleep 1
-	/usr/local/bin/gpio write $LIGHT_PIN 0
 	
 	# allow to exit infinite loop by pressing any key
 	read -t 1 -n 1 -s KEY
